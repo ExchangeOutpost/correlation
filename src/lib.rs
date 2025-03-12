09954628e2fc31b860b413701add96449e351e28
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-03-12 22:44:02 +0100

    Rename parameter in run function for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use extism_pdk::*;
 
 #[plugin_fn]
-pub fn run(name: String) -> FnResult<String> {
-    Ok(format!("Hello, {}!", name))
+pub fn run(data: String) -> FnResult<String> {
+    Ok(format!("Hello, {}!", data))
 }
