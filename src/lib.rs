b19044fdda5808bba9df5259b482b2030989cd19
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-03-13 09:15:03 +0100

    Add Serde support for FinData struct and update run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use extism_pdk::*;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+pub struct FinData {
+    data: String,
+}
 
 #[plugin_fn]
-pub fn run(data: String) -> FnResult<String> {
-    Ok(format!("Hello, {}!", data))
+pub fn run(data: FinData) -> FnResult<String> {
+    Ok(format!("Hello, {}!", data.data))
 }
