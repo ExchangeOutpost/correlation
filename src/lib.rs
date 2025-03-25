cdfe14e17622775c0e60af1bea1efa2c6092de16
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-03-25 21:52:24 +0100

    Refactor run function to support multiple data sets and calculate max, min, and avg for each symbol

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,24 @@
+use std::collections::HashMap;
+
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
 
 
 #[derive(Serialize, Deserialize)]
 pub struct FinData {
-    data: Vec<Vec<f64>>,
+    data: HashMap<String, Vec<Vec<f64>>>,
 }
 
 #[plugin_fn]
-pub fn run(data: Json<FinData>) -> FnResult<String> {
-    let data = data.into_inner();
-    let close_prices = data.data.iter().map(|x| x[3]).collect::<Vec<f64>>();
-    let max = close_prices.iter().cloned().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    let min = close_prices.iter().cloned().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    let avg = close_prices.iter().sum::<f64>() / close_prices.len() as f64;
-    let result = format!("Max: {}, Min: {}, Avg: {}", max, min, avg);
+pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
+    let fin_data = fin_data.into_inner();
+    let mut result = String::new();
+    for (key, value) in fin_data.data.iter() {
+        let close_prices = value.iter().map(|x| x[3]).collect::<Vec<f64>>();
+        let max = close_prices.iter().cloned().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+        let min = close_prices.iter().cloned().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+        let avg = close_prices.iter().sum::<f64>() / close_prices.len() as f64;
+        result = format!("{}\nSymbol: {}Max: {}, Min: {}, Avg: {}\n",key,result, max, min, avg);
+    }
     Ok(result)
 }
