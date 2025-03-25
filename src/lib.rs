0a81d690d57dd2fac65611c7b6f89886786779e5
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-03-25 22:00:34 +0100

    revert

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,19 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct FinData {
-    data: String,
+    data: HashMap<String, Vec<Vec<f64>>>,
 }
 
 #[plugin_fn]
 pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
-    // let fin_data = fin_data.into_inner();
-    // let mut result = String::new();
-    // for (key, value) in fin_data.data.iter() {
-    //     let close_prices = value.iter().map(|x| x[3]).collect::<Vec<f64>>();
-    //     let max = close_prices.iter().cloned().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    //     let min = close_prices.iter().cloned().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    //     let avg = close_prices.iter().sum::<f64>() / close_prices.len() as f64;
-    //     result = format!("{}\nSymbol: {}Max: {}, Min: {}, Avg: {}\n",key,result, max, min, avg);
-    // }
-    // Ok(result)
-    Ok(fin_data.into_inner().data.clone())
+    let fin_data = fin_data.into_inner();
+    let mut result = String::new();
+    for (key, value) in fin_data.data.iter() {
+        let close_prices = value.iter().map(|x| x[3]).collect::<Vec<f64>>();
+        let max = close_prices.iter().cloned().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+        let min = close_prices.iter().cloned().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
+        let avg = close_prices.iter().sum::<f64>() / close_prices.len() as f64;
+        result = format!("{}\nSymbol: {}Max: {}, Min: {}, Avg: {}\n",key,result, max, min, avg);
+    }
+    Ok(result)
 }
