b190d07349efc38b4ef47a1cdd621ca3a91d410a
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-03-26 00:06:40 +0100

    added correlation calculation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use ndarray::Array1;
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
 
@@ -9,6 +9,30 @@ pub struct FinData {
     data: HashMap<String, Vec<Vec<f64>>>,
 }
 
+/// Calculate the Pearson correlation coefficient between two stock price series
+fn pearson_correlation(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
+    let x_mean = x.mean().unwrap();
+    let y_mean = y.mean().unwrap();
+
+    let numerator = x.iter()
+        .zip(y.iter())
+        .map(|(&xi, &yi)| (xi - x_mean) * (yi - y_mean))
+        .sum::<f64>();
+
+    let denominator_x = x.iter()
+        .map(|&xi| (xi - x_mean).powi(2))
+        .sum::<f64>()
+        .sqrt();
+
+    let denominator_y = y.iter()
+        .map(|&yi| (yi - y_mean).powi(2))
+        .sum::<f64>()
+        .sqrt();
+
+    numerator / (denominator_x * denominator_y)
+}
+
+
 #[plugin_fn]
 pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
     let fin_data = fin_data.into_inner();
@@ -17,5 +41,9 @@ pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
     if symbols.len() != 2 {
         return Ok("Please provide exactly 2 symbols for correlation calculation".to_string());
     }
-    Ok("TODO".into())
+    let correlation=  pearson_correlation(
+        &Array1::from(candles_data.get(symbols[0]).unwrap().iter().map(|x| x[2]).collect::<Vec<f64>>()),
+        &Array1::from(candles_data.get(symbols[1]).unwrap().iter().map(|x| x[2]).collect::<Vec<f64>>()),
+    );
+    Ok(correlation.to_string())
 }
