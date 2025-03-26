c9dab5861817886120782e52a3303bb4c284d4a9
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-03-26 09:02:09 +0100

    explicit error handling

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ use serde::{Deserialize, Serialize};
 pub struct FinData {
     data: HashMap<String, Vec<Vec<f64>>>,
 }
+#[derive(Serialize, Deserialize)]
+pub struct Output {
+    correlation: f64,
+}
 
 /// Calculate the Pearson correlation coefficient between two stock price series
 fn pearson_correlation(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
@@ -37,13 +41,23 @@ fn pearson_correlation(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
 pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
     let fin_data = fin_data.into_inner();
     let candles_data: HashMap<String, Vec<Vec<f64>>> = fin_data.data;
-    let symbols = candles_data.keys().collect::<Vec<_>>();
-    if symbols.len() != 2 {
-        return Ok("Please provide exactly 2 symbols for correlation calculation".to_string());
-    }
+    let symbol_1 = match candles_data.get("symbol_1") {
+        Some(data) => data,
+        None => return Err(WithReturnCode::new(Error::new(std::io::Error::new(std::io::ErrorKind::Other, "symbol_1 not found")), -100)),
+    };
+
+    let symbol_2 = match candles_data.get("symbol_2") {
+        Some(data) => data,
+        None => return Err(WithReturnCode::new(Error::new(std::io::Error::new(std::io::ErrorKind::Other, "symbol_2 not found")), -100)),
+    };
+    
     let correlation=  pearson_correlation(
-        &Array1::from(candles_data.get(symbols[0]).unwrap().iter().map(|x| x[2]).collect::<Vec<f64>>()),
-        &Array1::from(candles_data.get(symbols[1]).unwrap().iter().map(|x| x[2]).collect::<Vec<f64>>()),
+        &Array1::from(symbol_1.iter().map(|x| x[2]).collect::<Vec<f64>>()),
+        &Array1::from(symbol_2.iter().map(|x| x[2]).collect::<Vec<f64>>()),
     );
-    Ok(correlation.to_string())
+    let out = Output {
+        correlation: correlation,
+    };
+    let function_response = serde_json::to_string(&out).unwrap();
+    Ok (function_response)
 }
