3ee1531b9b0823d560476356380371b7a88b3f49
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-03-29 11:41:06 +0100

    feat: implement custom deserialization for Candle struct and update SymbolsData to use Candle instances

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
 use extism_pdk::*;
 use ndarray::Array1;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct SymbolsData{
-    symbol_1: Vec<(i64, f64, f64, f64, f64, f64)>,
-    symbol_2: Vec<(i64, f64, f64, f64, f64, f64)>,
+/// Represents a single candlestick in financial data, typically used in trading charts.
+pub struct Candle {
+    /// The timestamp of the candlestick, usually in seconds since the Unix epoch.
+    pub timestamp: i64,
+    /// The opening price of the asset during the candlestick's time period.
+    pub open: f64,
+    /// The highest price of the asset during the candlestick's time period.
+    pub high: f64,
+    /// The lowest price of the asset during the candlestick's time period.
+    pub low: f64,
+    /// The closing price of the asset at the end of the candlestick's time period.
+    pub close: f64,
+    /// The trading volume of the asset during the candlestick's time period.
+    pub volume: f64,
+}
+// Custom deserialization from an array
+impl<'de> Deserialize<'de> for Candle {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let arr: (i64, f64, f64, f64, f64, f64) = Deserialize::deserialize(deserializer)?;
+        Ok(Candle {
+            timestamp: arr.0,
+            open: arr.1,
+            high: arr.2,
+            low: arr.3,
+            close: arr.4,
+            volume: arr.5,
+        })
+    }
 }
 
-#[derive(Serialize, Deserialize)]
+
+
+#[derive(Deserialize)]
+pub struct SymbolsData{
+    symbol_1: Vec<Candle>,
+    symbol_2: Vec<Candle>,
+}
+
+#[derive(Deserialize)]
 pub struct FinData {
     data: SymbolsData,
 }
@@ -46,8 +81,8 @@ pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
     let fin_data = fin_data.into_inner();
     
     let correlation=  pearson_correlation(
-        &Array1::from(fin_data.data.symbol_1.iter().map(|x| x.2).collect::<Vec<f64>>()),
-        &Array1::from(fin_data.data.symbol_2.iter().map(|x| x.2).collect::<Vec<f64>>()),
+        &Array1::from(fin_data.data.symbol_1.iter().map(|x| x.close).collect::<Vec<f64>>()),
+        &Array1::from(fin_data.data.symbol_2.iter().map(|x| x.close).collect::<Vec<f64>>()),
     );
     let out = Output {
         correlation: correlation,
