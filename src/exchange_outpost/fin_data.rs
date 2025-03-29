ed6a2f356915e748831aa591dacce35bb4576e87
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-03-29 20:27:19 +0100

    feat: add Candle and FinData structs with custom deserialization for financial data

---
+++ src/exchange_outpost/fin_data.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use crate::exchange_outpost::Candle;
+use serde::Deserialize;
+use serde::de::DeserializeOwned;
+use extism_pdk::*;
+use extism_pdk::FromBytesOwned;
+
+#[derive(Deserialize)]
+pub struct FinData<T> {
+    data: HashMap<String, Vec<Candle<T>>>,
+}
+
+impl<T> FromBytesOwned for FinData<T>
+where
+    T: DeserializeOwned,
+{
+    fn from_bytes_owned(bytes: &[u8]) -> Result<Self, extism_pdk::Error> {
+        Ok(serde_json::from_slice(bytes)?)
+    }
+}
+impl<T> FinData<T> {
+    pub fn get_symbol_data(&self, symbol: &str) -> Result<&Vec<Candle<T>>, WithReturnCode<Error>> {
+        self.data.get(symbol).ok_or(
+            WithReturnCode::new(Error::new(std::io::Error::new(std::io::ErrorKind::Other, format!(
+            "Symbol {} not found", symbol
+        ))), -1))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,10 @@
-use extism_pdk::*;
+mod exchange_outpost;
+use crate::exchange_outpost::FinData;
+use extism_pdk::{plugin_fn, FnResult};
 use ndarray::Array1;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::Serialize;
 
-/// Represents a single candlestick in financial data, typically used in trading charts.
-pub struct Candle {
-    /// The timestamp of the candlestick, usually in seconds since the Unix epoch.
-    pub timestamp: i64,
-    /// The opening price of the asset during the candlestick's time period.
-    pub open: f64,
-    /// The highest price of the asset during the candlestick's time period.
-    pub high: f64,
-    /// The lowest price of the asset during the candlestick's time period.
-    pub low: f64,
-    /// The closing price of the asset at the end of the candlestick's time period.
-    pub close: f64,
-    /// The trading volume of the asset during the candlestick's time period.
-    pub volume: f64,
-}
-// Custom deserialization from an array
-impl<'de> Deserialize<'de> for Candle {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let arr: (i64, f64, f64, f64, f64, f64) = Deserialize::deserialize(deserializer)?;
-        Ok(Candle {
-            timestamp: arr.0,
-            open: arr.1,
-            high: arr.2,
-            low: arr.3,
-            close: arr.4,
-            volume: arr.5,
-        })
-    }
-}
-
-
-
-#[derive(Deserialize)]
-pub struct SymbolsData{
-    symbol_1: Vec<Candle>,
-    symbol_2: Vec<Candle>,
-}
-
-#[derive(Deserialize)]
-pub struct FinData {
-    data: SymbolsData,
-}
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 pub struct Output {
     correlation: f64,
 }
@@ -75,18 +32,14 @@ fn pearson_correlation(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
     numerator / (denominator_x * denominator_y)
 }
 
-
 #[plugin_fn]
-pub fn run(fin_data: Json<FinData>) -> FnResult<String> {
-    let fin_data = fin_data.into_inner();
-    
+pub fn run(fin_data: FinData<f64>) -> FnResult<String> {
     let correlation=  pearson_correlation(
-        &Array1::from(fin_data.data.symbol_1.iter().map(|x| x.close).collect::<Vec<f64>>()),
-        &Array1::from(fin_data.data.symbol_2.iter().map(|x| x.close).collect::<Vec<f64>>()),
+        &Array1::from_iter(fin_data.get_symbol_data("symbol_1")?.iter().map(|x| x.close)),
+        &Array1::from_iter(fin_data.get_symbol_data("symbol_2")?.iter().map(|x| x.close)),
     );
     let out = Output {
         correlation: correlation,
     };
-    let function_response = serde_json::to_string(&out).unwrap();
-    Ok (function_response)
+    Ok(serde_json::to_string(&out).unwrap())
 }
