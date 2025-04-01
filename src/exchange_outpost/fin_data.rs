7f7510777b0a6ba62c4a57a42ef198b10f43e8ac
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-04-01 14:00:07 +0200

    feat: add methods to retrieve pipe sources and data from pipe in FinData

--- src/exchange_outpost/fin_data.rs
+++ src/exchange_outpost/fin_data.rs
@@ -20,11 +20,24 @@ where
         Ok(serde_json::from_slice(bytes)?)
     }
 }
+
+#[allow(dead_code)]
 impl<T> FinData<T> {
     pub fn get_candles(&self, symbol: &str) -> Result<&Vec<Candle<T>>, WithReturnCode<Error>> {
         self.tickers_data.get(symbol).ok_or(
             WithReturnCode::new(Error::new(std::io::Error::new(std::io::ErrorKind::Other, format!(
             "Symbol {} not found", symbol
         ))), 1))
+    }
+
+    pub fn get_pipe_sources (&self) -> Vec<&String> {
+        self.piped_data.keys().collect()
+    }
+
+    pub fn get_data_from_pipe (&self, source: &str) -> Result<&String, WithReturnCode<Error>> {
+        self.piped_data.get(source).ok_or(
+            WithReturnCode::new(Error::new(std::io::Error::new(std::io::ErrorKind::Other, format!(
+            "Source {} not found", source
+        ))), 2))
     }
 }
